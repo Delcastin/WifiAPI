1472143793f0955c46823e4d277a3002f97a440b
Delcastin <lejesu96@naver.com>
2025-05-08 17:59:24 +0900

    First Commit for OpenAPI Wifi Project.

---
+++ src/test/java/com/zerobase/wifiapi/WifiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.wifiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
